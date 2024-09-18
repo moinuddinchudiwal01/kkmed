730f4a027b8bfca58d2a614b1db29cc2e825466c
Kasim-bigshorts <Kasim.Kadiwala@bigshorts.co>
2024-09-18 08:27:46 +0530

    changes in homescreen

--- core/helper/helper.ts
+++ core/helper/helper.ts
@@ -8,7 +8,7 @@ export const getIconName = (
     Home: focused ? 'home' : 'home-outline',
     Upload: focused ? 'cloud-upload' : 'cloud-upload-outline',
     Order: focused ? 'cube' : 'cube-outline',
-    Profile: focused ? 'person' : 'person-outline',
+    Profile: focused ? 'person-circle-outline' : 'person-circle',
   };
 
   return icons[routeName] || 'home-outline';
