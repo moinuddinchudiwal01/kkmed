8b75c8c271e5f292fbde9d985d10448246bc121f
Moinuddin <moinuddinchudiwal@gmail.com>
2024-09-20 09:43:25 +0530

    category and produt dummy data added

---
+++ data/categoryData.tsx
@@ -0,0 +1,58 @@
+export const category = [
+  {
+    id: '1',
+    title: 'Medicine',
+    category_image: 'https://example.com/images/medicine-category.png',
+    description:
+      'Medicinal and pharmaceutical products including tablets, syrups, and injections.',
+  },
+  {
+    id: '2',
+    title: 'Healthcare',
+    category_image: 'https://example.com/images/healthcare-category.png',
+    description:
+      'Healthcare essentials including wellness products, first-aid, and health monitoring devices.',
+  },
+  {
+    id: '3',
+    title: 'Insulin',
+    category_image: 'https://example.com/images/insulin-category.png',
+    description:
+      'Diabetic care products including insulin injections and blood sugar testing kits.',
+  },
+  {
+    id: '4',
+    title: 'Skincare',
+    category_image: 'https://example.com/images/skincare-category.png',
+    description:
+      'Products for skincare, including creams, moisturizers, and treatments for various skin conditions.',
+  },
+  {
+    id: '5',
+    title: 'Food',
+    category_image: 'https://example.com/images/food-category.png',
+    description:
+      'Nutritional food products including health supplements, vitamins, and organic food items.',
+  },
+  {
+    id: '6',
+    title: 'Fitness',
+    category_image: 'https://example.com/images/fitness-category.png',
+    description:
+      'Fitness and exercise products including equipment, supplements, and workout gear.',
+  },
+  {
+    id: '7',
+    title: 'Homecare',
+    category_image: 'https://example.com/images/homecare-category.png',
+    description:
+      'Homecare products including disinfectants, cleaners, and other household essentials.',
+  },
+  {
+    id: '8',
+    title: 'Ayurveda',
+    category_image: 'https://example.com/images/ayurveda-category.png',
+    description:
+      'Traditional Ayurvedic medicines and herbal products for natural health care solutions.',
+  },
+];
